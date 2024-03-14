1380a8fc0b4ccb72e24b55311279e23a05aee564
lukasdchang <lukaschang.d@gmail.com>
2024-03-14 16:44:01 -0400

    ;aiojfafie

---
+++ spchk.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+
+void listFiles(const char* dirname){
+    DIR* dir = opendir(dirname);
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+    struct dirent* entity;
+    entity = readdir(dir);
+    while (entity != NULL) {
+        printf("%hhd &s/&s\n", entity->d_type, dirname, entity->d_name);
+        if(entity->d_type == DT_DIR && strcmp(entity->d_name, ".") != 0 && strcmp(entity->d_name, "..") != 0){
+            char path[100] = { 0 };
+            strcat(path, dirname);
+            strcat(path, "/");
+            strcat(path, entity->d_name);
+            listFiles(path);
+        }
+        entity = readdir(dir);
+    }
+    closedir(dir);
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
+        return 1;
+    }
+    listFiles(argv[1]);
+    return 0;
+}
